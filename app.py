c5e92cbe755ed4a7f6bd66bd5cebe201fd0e3941
ATrueNovice <sean.haughtonscott@gmail.com>
2022-10-14 10:29:00 -0400

    missing results, details, and fonts

--- app.py
+++ app.py
@@ -12,6 +12,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return  render_template('index.html')
+@app.route('/login')
+def login():
+    return  render_template('login.html')
+@app.route('/home', methods=['POST'])
+def home():
+    return  render_template('index.html')
+
+@app.route('/search')
+def search():
+    return  render_template('enhanced.html')
+
+@app.route('/calendar')
+def calendar():
+    return  render_template('calendar.html')
+
+@app.route('/jira')
+def jira():
+    return  render_template('jira.html')
 
 
 
